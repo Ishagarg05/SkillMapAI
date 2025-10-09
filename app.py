78fe792e3ba124e184e3c3b44c37b0f7d8dc57ee
Isha garg <Ishagarg2425@gmail.com>
2025-10-09 17:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,4 @@
 # import streamlit as st
-# import Home
-# import Plan_My_Journey
-# import Saved
-# import Firebasedb
-
-# st.set_page_config(page_title="SkillMapAI", layout="wide")
-
-# # Sidebar ek hi jagah
-# st.sidebar.title("SkillMapAI")
-# st.sidebar.subheader("Navigate")
-
-# page = st.sidebar.radio("Go to", ["Home", "Account", "Plan My Journey", "Saved Roadmaps"])
-
-# # Page render logic
-# if page == "Home":
-#     Home.app()
-# elif page == "Account":
-#     Firebasedb.app()
-# elif page == "Plan My Journey":
-#     Plan_My_Journey.app()
-# elif page == "Saved Roadmaps":
-#     Saved.app()
 import streamlit as st
 import Home
 import Plan_My_Journey
